484e0bc9c9d5d7ec20b691b409465f8342f03769
maluda <nwitevictor7@gmail.com>
2025-07-03 17:29:51 +0100

    Initial commit from Create Next App

---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+export const APP_NAME = process.env.APP_NAME || "NFT Marketplace";
+
+export const APP_SLOGAN =
+  process.env.APP_SLOGAN || "Buy, Sell and Own Digital Assets Securely";
+
+export const APP_DESCRIPTION =
+  process.env.APP_DESCRIPTION ||
+  "A decentralized NFT marketplace built with Solidity smart contracts. Create, list and trade NFTs securely and transparently on the Ethereum blockchain.";
