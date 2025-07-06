6b50964d1bb4e6295da973f17bd495edc7721b79
maluda <nwitevictor7@gmail.com>
2025-07-06 03:45:17 +0100

    done

--- components/shared/Navbar/Notification.tsx
+++ components/shared/Navbar/Notification.tsx
@@ -1,3 +1,25 @@
+import Image from "next/image";
+import image from "@/public/assets/image";
+
 export const Notification = () => {
-  return <div></div>;
+  return (
+    <div className="absolute p-4 shadow-xl top-15 border rounded-md bg-background z-100 w-64 text-lg">
+      <p className="text-xl font-semibold mb-8"></p>
+      <div className="rounded-md">
+        <Image
+          src={image.user1}
+          alt="profile"
+          width={50}
+          height={50}
+          className="object-cover rounded-full"
+        />
+      </div>
+      <div className="">
+        <h4>Maludatech</h4>
+        <p>Address </p>
+        <small>3 minutes ago</small>
+        <span className=""></span>
+      </div>
+    </div>
+  );
 };
