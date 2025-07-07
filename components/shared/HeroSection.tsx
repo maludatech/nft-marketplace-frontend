6b6e31fecec4f42ff6c2e955e3491a1058d093b0
maluda <nwitevictor7@gmail.com>
2025-07-07 03:08:37 +0100

    added image to heros ecsection

--- components/shared/HeroSection.tsx
+++ components/shared/HeroSection.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import image from "@/public/assets/image";
 import { Button } from "../ui/button";
+import { Search } from "lucide-react";
 
 export const HeroSection = () => {
   return (
@@ -10,8 +11,23 @@ export const HeroSection = () => {
           <h1 className="text-4xl font-bold text-foreground">
             Discover, collect, and sell NFTs 🖼️
           </h1>
+          <p className="text-muted-foreground">
+            Discover the most outstanding NTFs in all topics of life. Creative
+            your NTFs and sell them
+          </p>
+          <Button className="rounded-full flex items-center gap-2 text-[16px] cursor-pointer px-4 py-6.5 w-fit text-white">
+            Start your search <Search />
+          </Button>
         </div>
-        <div></div>
+        <div className="flex flex-col gap-4">
+          <Image
+            src={image.hero}
+            alt="hero"
+            width={600}
+            height={600}
+            className="object-cover"
+          />
+        </div>
       </div>
     </div>
   );
