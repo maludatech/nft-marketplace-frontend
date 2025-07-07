64e95d931d2ec28ec072a10e9049b5209260af4d
maluda <nwitevictor7@gmail.com>
2025-07-07 18:07:25 +0100

    updated services compoent

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import { HeroSection } from "@/components/shared/HeroSection";
+import { Services } from "@/components/shared/Services";
 
 export default function Home() {
-  return <HeroSection />;
+  return (
+    <>
+      <HeroSection />
+      <Services />
+    </>
+  );
 }
--- components/shared/Services.tsx
+++ components/shared/Services.tsx
@@ -1,11 +1,52 @@
+import Image from "next/image";
+import image from "@/public/assets/image";
+
 export const Services = () => {
+  const services = [
+    {
+      image: image.service1,
+      title: "Filter & Discover",
+      description: "Smart filtering and suggestions make it easy to find",
+    },
+    {
+      image: image.service2,
+      title: "Add to bag",
+      description: "Easily select the correct items and add them to the cart",
+    },
+    {
+      image: image.service3,
+      title: "Fast shipping",
+      description: "The carrier will confirm and ship quickly to you",
+    },
+    {
+      image: image.service4,
+      title: "Enjoy the product",
+      description: "Have fun and enjoy your 5-star quality products",
+    },
+  ];
   return (
-    <div className="flex flex-col gap-4">
-      <h1 className="text-4xl font-bold text-foreground">Services</h1>
-      <p className="text-muted-foreground text-lg sm:text-xl">
-        Discover the most outstanding NTFs in all topics of life. Creative your
-        NTFs and sell them
-      </p>
+    <div className="w-full min-h-screen">
+      <div className="body-container py-36 px-12 grid sm:grid-cols-2 md:grid-cols-4 place-items-start gap-16 sm:gap-8 w-full">
+        {services.map((service, index) => (
+          <div
+            className="flex flex-col gap-4 justify-center items-center w-full"
+            key={index}
+          >
+            <Image
+              src={service.image}
+              alt="service"
+              width={100}
+              height={100}
+              className="object-contain"
+            />
+            <div className="text-primary">Step {index + 1}</div>
+            <h1 className="font-semibold">{service.title}</h1>
+            <p className="text-muted-foreground text-sm text-center">
+              {service.description}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
