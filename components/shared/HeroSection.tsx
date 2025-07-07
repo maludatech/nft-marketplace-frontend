544c5cc1f3cc7a8598b266de334802d519a680c9
maluda <nwitevictor7@gmail.com>
2025-07-07 02:57:28 +0100

    updated hero section

--- components/shared/HeroSection.tsx
+++ components/shared/HeroSection.tsx
@@ -3,5 +3,16 @@ import image from "@/public/assets/image";
 import { Button } from "../ui/button";
 
 export const HeroSection = () => {
-  return <div className="py-24 px-12 w-full"></div>;
+  return (
+    <div className="w-full h-screen">
+      <div className="body-container pt-48 pb-12 px-12 flex flex-col sm:flex-row gap-4">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-4xl font-bold text-foreground">
+            Discover, collect, and sell NFTs 🖼️
+          </h1>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
