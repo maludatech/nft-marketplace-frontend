1e3f919a4bdc48c7caa65193dce9413e755c99cf
maluda <nwitevictor7@gmail.com>
2025-07-18 02:39:49 +0100

    added rainbow kit UI styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
 import { APP_NAME, APP_SLOGAN, APP_DESCRIPTION } from "@/lib/constants";
 import { Toaster } from "@/components/ui/sonner";
 import { Footer } from "@/components/shared/Footer";
